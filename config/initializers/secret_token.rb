06a86189fec2e1c6d98870224e5057dc0d2929f8
Kenny Chung <kenny@uchicago.edu>
2013-05-15 23:18:02 -0500

    omniauth and figaro installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatingApp::Application.config.secret_token = '09b57356c6a0f2e5fada37ae89f0c5a9a5591aa306318a70c6790edc7a1abba23ce078c5b57c926e13e3fe6290b251b23875e703a8a37e48fb07a6a3e1521971'
